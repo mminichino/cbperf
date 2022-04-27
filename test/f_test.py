4a7b1165df0c8d884a527b66dbb41da8b5b20a0f
Michael Minichino <mminichino@mac.com>
2022-04-26 20:26:41 -0500

    1.1 development

--- test/f_test.py
+++ test/f_test.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env -S python3 -W ignore
 
+import os
+import sys
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
 from lib import system
 
 
